e3611142a9e6e341c1e393f07de57d0c2236ad4c
Jazlynn Liu <jazlynnliu@gmail.com>
2024-10-28 19:41:21 -0500

    working reading encoders code

--- raspi_robot_controls.py
+++ raspi_robot_controls.py
@@ -1,12 +1,12 @@
 from dual_max14870_rpi import motors, MAX_SPEED
-import encoders
+import Encoder
 
 robot_turn = False
 MOTOR_MAX_SPEED = motors.MAX_SPEED
+enc = Encoder.Encoder(5,6) # 5 and 6 are pin numbers
 
 # tracking all previous distances from encoders
-prev_x = 0
-prev_y = 0
+prev_loc = 0
 # set if robot is using x/o somewhere
 
 ### tic tac toe logic ###
@@ -18,22 +18,20 @@ def tic_tac_toe_logic():
     board = [[0,0,0],[0,0,0],[0,0,0]]
     x = 0
     y = 0
+    return x
 
 ### move robot when it's its turn to move ###
 if robot_turn:
-    [x_enc, y_enc] = encoders.readEncoders() # current location
-    [next_x, next_y] = tic_tac_toe_logic()
+    loc_enc = enc.read()
+    next_loc = tic_tac_toe_logic()
     motors.enable()
 
     # current robot position + updating previous position
-    cur_x = x_enc - prev_x
-    cur_y = y_enc - prev_y
-    prev_x = x_enc
-    prev_y = x_enc
+    cur_loc = loc_enc - prev_loc
 
     # distance from desired position
-    dist_x = next_x - cur_x
-    dist_y = next_y - cur_y
+    dist_x = next_loc - cur_loc
+    dist_y = 0
 
     x_speed = dist_x * 5 # idk some random int
     y_speed = dist_y * 5
