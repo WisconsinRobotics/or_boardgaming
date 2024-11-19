f9268fe6431de7e75256897bdffbaea88514f2c2
jazlynn-liu <jazlynnliu@gmail.com>
2024-11-18 16:11:33 -1000

    functional closed-loop control!

--- raspi_robot_controls.py
+++ raspi_robot_controls.py
@@ -1,9 +1,9 @@
 from dual_max14870_rpi import motors, MAX_SPEED
 import Encoder
 
-robot_turn = False
+robot_turn = True
 MOTOR_MAX_SPEED = motors.MAX_SPEED
-enc = Encoder.Encoder(5,6) # 5 and 6 are pin numbers
+enc = Encoder.Encoder(20, 21) # 5 and 6 are pin numbers
 
 # tracking all previous distances from encoders
 prev_loc = 0
@@ -21,18 +21,20 @@ def tic_tac_toe_logic():
     return x
 
 ### move robot when it's its turn to move ###
-if robot_turn:
-    loc_enc = enc.read()
-    next_loc = tic_tac_toe_logic()
-    motors.enable()
+while 1:
+    if robot_turn:
+        loc_enc = enc.read()
+        next_loc = tic_tac_toe_logic()
+        motors.enable()
+        print(loc_enc)
+        # current robot position + updating previous position
+        cur_loc = loc_enc - prev_loc
 
-    # current robot position + updating previous position
-    cur_loc = loc_enc - prev_loc
+        # distance from desired position
+        dist_x = 50 - loc_enc
+        dist_y = 0
 
-    # distance from desired position
-    dist_x = next_loc - cur_loc
-    dist_y = 0
-
-    x_speed = dist_x * 5 # idk some random int
-    y_speed = dist_y * 5
-    motors.setSpeeds(x_speed, y_speed)
+        x_speed = dist_x * 0.05 # idk some random int
+        y_speed = dist_y * 0.05
+        print("x speed", x_speed)
+        motors.setSpeeds(x_speed, y_speed)
